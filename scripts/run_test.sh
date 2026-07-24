bf9697eb7837be207d01909c69ac443c61c7ee92
HwangChul <2678216433@qq.com>
2026-07-24 10:57:03 +0800

    test ablation

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -9,11 +9,12 @@ conda activate imperio
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR/.."
 
-epochs=("20" "30" "40" "50" "60" "70")
+ablations=("0.1,20" "1,5" "1,10" "1,50" "5,20" "10,20" "wo_teb" "wo_ceb" "wo_clma")
+path="ablation/"
 
-for epoch in "${epochs[@]}"
+for ablation in "${ablations[@]}"
 do
-    echo "正在测试 Epoch: $epoch"
-    python test_LLVIP.py --pth_epoch "$epoch"
+    echo "正在测试 ablation: $ablation"
+    python test_LLVIP.py --model_path $path$ablation
     echo "--------------------------"
 done
